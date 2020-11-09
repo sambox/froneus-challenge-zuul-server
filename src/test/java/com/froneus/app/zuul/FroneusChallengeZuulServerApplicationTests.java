a31c85e1eb38383b69218a5de76a0fce33ff48d2
Sebastián Molina <smolina.sbs@gmail.com>
2020-11-09 02:16:27 -0300

    initial commit

---
+++ src/test/java/com/froneus/app/zuul/FroneusChallengeZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froneus.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FroneusChallengeZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
